609caf52246ded5d0bd14856d60ba17f922cc3b9
MOHAMMAD TALHA ALI <talhaali0006969@gmail.com>
2023-11-21 11:52:21 +0500

    Initial Commit

--- lib/const/strings.dart
+++ lib/const/strings.dart
@@ -1,8 +1,33 @@
-const appname = "eMart Seller App",
+const appname = "ShopePerfecto App",
     login = "Login",
     welcome = "Hi, Welcome to",
     email = "Email",
     password = "Password",
-    forgotPassword = "Forgot Password",
+    emailHint = "eg. vendor@emart.com",
+    passwordHint = "* * * * * *",
+    logout = "Logout",
+    loginTo = " Login to your account",
+    forgotPassword = "Forgot Password ? ",
+    confirmPass = "Confirm Password",
     anyProblem = "In case of any difficulty, contact administration",
-    credit = "@BaabaDevs";
+    credit = "@CoolDudesDevs",
+    dashboard = "Dashboard",
+    orders = 'Orders',
+    products = 'Products',
+    rating = "Rating",
+    totalsale = "Total Sales",
+    popularProducts = "Popular Products",
+    unpaid = "Unpaid",
+    settings = "Settings";
+// more
+const featured = "Featured", edit = "Edit", remove = "Remove";
+// admin
+
+const shopSettings = "Shop Settings",
+    messages = "Messages",
+    sliders = "Sliders Settings",
+    save = "Save",
+    editProfile = "Edit Your Profile";
+// messages screen
+
+const chat = "Chats";
